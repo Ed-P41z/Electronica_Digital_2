b8c4fe285925493da7de64c1f9322ea14c18bdc0
Ed-P41z <pai23072@uvg.edu.gt>
2026-01-22 01:37:25 -0600

    Ejemplo en clase
    Ejemplo en clase

---
+++ Ejemplos en clase/Ejemplo_DisplayLCD/Ejemplo_DisplayLCD/main.c
@@ -0,0 +1,21 @@
+/*
+ * Ejemplo_DisplayLCD.c
+ *
+ * Created: 1/21/2026 4:29:05 PM
+ * Author : edvin
+ */ 
+
+#include <avr/io.h>
+
+
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
