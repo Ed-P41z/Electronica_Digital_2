9db8c2ffe31515a6defcd0647ff9091aa2f03ac5
Ed-P41z <pai23072@uvg.edu.gt>
2026-01-22 01:38:06 -0600

    Se creó el documento para el lab02
    Se creó el documento con la finalidad de empezar a desarrollar la librería correspondiente para el display LCD

---
+++ Lab02/Lab02/Lab02/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab02.c
+ *
+ * Created: 1/22/2026 1:18:58 AM
+ * Author : edvin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
